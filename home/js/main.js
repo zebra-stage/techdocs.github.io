11e12213c9d6db837342cfe71b4b873fd846b424
nrcg87 <40235943+nrcg87@users.noreply.github.com>
2024-10-25 08:06:40 -0400

    [DLM-39328] Publish DTRK 6.3

--- home/js/main.js
+++ home/js/main.js
@@ -865,6 +865,7 @@ function populateDropdown (ddl_ver, ddl_doc) {
             }
             break;
         case '/devicetracker/6-2':
+        case '/devicetracker/6-3':
             ddl_doc.options.length = 0;
             for (i=0; i < dt_6_2_text.length; i++) {
                 createOption(ddl_doc, dt_6_2_text[i], dt_6_2_val[i]);
